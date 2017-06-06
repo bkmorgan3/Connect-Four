49dc70b8893b8906140faa97796504de66845b4f
brad morgan <bkmorgan3@gmail.com>
2017-06-06 15:18:54 -0700

    some win logic

--- js/main.js
+++ js/main.js
@@ -24,6 +24,10 @@ var $checker = $('.checker');
 // console.log($checker);
 $checker.on('click', handleClick);
 
+// the reset button and click listener
+var $reset = $('#reset');
+$reset.click(resetGame);
+
 // function instructions delegating current player and switching the player.
 function nextPlayer(){
   if(players.currentPlayer == players.player1){
@@ -42,7 +46,8 @@ function handleClick(){
   console.log("row", row)
   data[row][column] = players.currentPlayer
   console.log('data',data)
-
+  console.log('data row:',data[row][column]);
+  // .data([row==3][col==5])
   //sets color of checker to be played according to current player
   if (players.currentPlayer == players.player1) {
     $(this).css('backgroundColor', players.player1);
@@ -52,9 +57,13 @@ function handleClick(){
     $(this).css('backgroundColor', players.player2);
     $(this).addClass('filled')
   }
+
+  checkHorizontalWin(row, column)
+  checkVerticalWinner(row, column)
   // function to call the next player
   nextPlayer();
   $(this).off('click');
+  // horizontalWinCheck();
 }
 
 
@@ -63,12 +72,58 @@ function resetGame(){
   $checker.css('backgroundColor', 'white');
 }
 
-var $reset = $('#reset');
-$reset.click(resetGame);
 
+// once win conditons are met this will display winner and lock board
+function weHaveAWinner(){
+  var $h1 = $('h1').html(players.currentPlayer + ' wins !!!');
+  lockBoard();
+}
 
+// checks for horizontal wins
+function checkHorizontalWin(row, column){
+  numInARow = 1
+  currentRow = row
+  currentColumn = column
+
+  // check for match horizontal/left:
+  // Check one over to the left:
+  if (data[currentRow][currentColumn -1] === players.currentPlayer && currentColumn -1 >= 0  ){
+      // Increase number in a row count:
+      numInARow++
+
+      console.log(numInARow + " in a row")
+
+      // Check next one over to the left or right:
+      if (data[currentRow][currentColumn -2 ] === players.currentPlayer && currentColumn -2 >= 0  ){
+        numInARow++
+        console.log(numInARow + " in a row")
+        if(data[currentRow][currentColumn -3 ] === players.currentPlayer && currentColumn -3 >= 0 ){
+            weHaveAWinner();
+        }
+      }
+  }
+}
+
+function checkVerticalWinner(row, column){
+  // numInARow = 1
+  // currentRow = row
+  // currentColumn = column
+  // // check for match vertical
+  // if(data[currentColumn][currentRow -1 || currentRow +1] === players.currentPlayer && currentRow -1 >=0){
+  //   numInARow++
+  //
+  //   console.log('vert' ,numInARow + " in a row")
+  //
+  //   if(data[currentColumn][currentRow -2 || currentRow +2]=== players.currentPlayer && currentRow -2 >=0){
+  //     numInARow++
+  //     console.log('vert', numInARow + " in a row");
+  //     if(data[currentColumn][currentRow -3 || currentRow + 3] === players.currentPlayer && currentRow -3 >=0 ){
+  //       weHaveAWinner();
+  //     }
+  //   }
+  // }
+}
 
 // veritcal checker -> responsible for checking divs in 7's
 // diagnoal checker -> responsible for checking divs in 6's
-// horizontal checker -> responsible for checking div next to current div (or by 1)
 // a checker to place the element at the bottom-most position that is not occupied
